7ba5b4189a6c11ff4b6d264a91f927949eafc620
alex-vo <avoroncovs@gmail.com>
2014-01-30 13:47:40 +0100

    Update db.sql

--- db.sql
+++ db.sql
@@ -29,6 +29,8 @@ CREATE TABLE `user` (
   `dropbox_access_secret` varchar(255) DEFAULT NULL,
   `dropbox_request_key` varchar(255) DEFAULT NULL,
   `dropbox_request_secret` varchar(255) DEFAULT NULL,
+  `drive_access_token` varchar(255) DEFAULT NULL,
+  `drive_refresh_token` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
