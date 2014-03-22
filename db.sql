b23fb483840a642d5657a124e869b4e10947d9b1
vanstr <mihnenok@gmail.com>
2014-03-22 14:06:31 +0200

    Sql Dump added

--- db.sql
+++ db.sql
@@ -1,7 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.31, for debian-linux-gnu (i686)
--- mysql -u admin -p123 cloud_player < db.sql
+-- MySQL dump 10.11
+--
 -- Host: localhost    Database: cloud_player
 -- ------------------------------------------------------
+-- Server version	5.0.96-community-nt
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -15,6 +16,42 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `song`
+--
+
+DROP TABLE IF EXISTS `song`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `song` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
+  `cloud_id` bigint(20) default NULL,
+  `file_name` varchar(255) default NULL,
+  `file_size` bigint(20) default NULL,
+  `last_time_accessed` datetime default NULL,
+  `metadata_album` varchar(255) default NULL,
+  `metadata_artist` varchar(255) default NULL,
+  `metadata_genre` varchar(255) default NULL,
+  `metadata_length_seconds` int(11) default NULL,
+  `metadata_title` varchar(255) default NULL,
+  `metadata_year` varchar(255) default NULL,
+  PRIMARY KEY  (`id`),
+  KEY `FK35F515AB25946C` (`user_id`),
+  CONSTRAINT `FK35F515AB25946C` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `song`
+--
+
+LOCK TABLES `song` WRITE;
+/*!40000 ALTER TABLE `song` DISABLE KEYS */;
+INSERT INTO `song` VALUES (1,1,1,'Shots.mp3',0,NULL,'www.VanStation.blogspot.com','www.VanStation.blogspot.com',NULL,0,NULL,NULL);
+/*!40000 ALTER TABLE `song` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `user`
 --
 
@@ -23,15 +60,18 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `login` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `dropbox_access_key` varchar(255) DEFAULT NULL,
-  `dropbox_access_secret` varchar(255) DEFAULT NULL,
-  `dropbox_request_key` varchar(255) DEFAULT NULL,
-  `dropbox_request_secret` varchar(255) DEFAULT NULL,
-  `drive_access_token` varchar(255) DEFAULT NULL,
-  `drive_refresh_token` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `login` varchar(255) default NULL,
+  `password` varchar(255) default NULL,
+  `drive_access_token` varchar(255) default NULL,
+  `drive_refresh_token` varchar(255) default NULL,
+  `dropbox_access_key` varchar(255) default NULL,
+  `dropbox_access_secret` varchar(255) default NULL,
+  `dropbox_request_key` varchar(255) default NULL,
+  `dropbox_request_secret` varchar(255) default NULL,
+  `drive_token_expires` bigint(20) default NULL,
+  `dropbox_uid` varchar(255) default NULL,
+  `google_email` varchar(255) default NULL,
+  PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -41,7 +81,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO cloud_player.user (id, login, password, dropbox_access_key, dropbox_access_secret, dropbox_request_key, dropbox_request_secret) VALUES (1, 'test', '123', 'k7iw85bfyrbm3vwr', 'f1zcl5re9zzoyo9', 'nnCdh4A14OEWmM5c', 'xmiOKgf7JIy75a5V');
+INSERT INTO `user` VALUES (1,'test','123',NULL,NULL,'BAus-dLEjW8AAAAAAAAAAVDysztTsSGkiwlJV7Fm6lvHYxbp0-QdBsyE_Hb_7dYd','7hlztwsgm4v8l2f','C6jC5Vm8aiRDiNwy','BLOUNZc72TGN6Aq9',NULL,'192670402',NULL);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -53,3 +93,5 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-03-22 13:51:46
