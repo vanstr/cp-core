5ebf6ae19880249ed44b0dbfe0d9007d5510ac3e
vanstr <mihnenok@gmail.com>
2014-09-11 22:53:48 +0300

    Play application base

---
+++ app/controllers/AuthorizationApi.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import play.mvc.Controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vanstr
+ * Date: 14.11.9
+ * Time: 20:48
+ * To change this template use File | Settings | File Templates.
+ */
+public class AuthorizationApi extends Controller {
+}
