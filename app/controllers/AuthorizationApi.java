711f7af9ac20987937464a700fc5ee572fa36944
Alex <avoroncovs@gmail.com>
2014-09-24 11:51:16 +0300

    Added Dropbox/Gdrive account removal functionality.

--- app/controllers/AuthorizationApi.java
+++ app/controllers/AuthorizationApi.java
@@ -122,4 +122,24 @@ public class AuthorizationApi extends BaseController {
 
         return redirect("/");
     }
+
+    public static Result removeDrive(){
+        Long userId = Long.parseLong(session("user"));
+        User user = User.getUserById(userId);
+        user.driveAccessToken = null;
+        user.driveRefreshToken = null;
+        user.googleEmail = null;
+        user.driveTokenExpires = null;
+        user.update();
+        return ok();
+    }
+
+    public static Result removeDropbox(){
+        Long userId = Long.parseLong(session("user"));
+        User user = User.getUserById(userId);
+        user.dropboxAccessKey = null;
+        user.dropboxUid = null;
+        user.update();
+        return ok();
+    }
 }
